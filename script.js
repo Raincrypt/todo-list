ce2ab6fa1e45aaf4d72f6fa570dac70002bd8544
Mousom <sarkarmousom8@gmail.com>
2023-07-13 22:16:25 +0530

    Basic Todo List Completed

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ let addTask = () => {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.append(li);
-        li.classList.add("task")
+        li.classList.add("task");
+        inputBox.clear;
 
         let deleteIcon = document.createElement("span");
         deleteIcon.innerHTML = "\u00d7";
@@ -16,4 +17,26 @@ let addTask = () => {
     else{
         alert("You must write something")
     }
+    inputBox.value = "";
+    saveData();
 }
+
+listContainer.addEventListener("click", (e)=>{
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false)
+
+let saveData = () =>{
+    localStorage.setItem("data", listContainer.innerHTML)
+}
+
+let getData = () => {
+    listContainer.innerHTML = localStorage.getItem("data")
+}
+getData();
